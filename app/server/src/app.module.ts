e131a0566fda4d07f8913ec506442ea3c79c018d
mamblijoe <YtL6xgfr7sxduA>
2023-04-22 23:13:51 +0300

    feat(starter): db mysql

--- app/server/src/app.module.ts
+++ app/server/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'db',
+      password: 'db',
+      database: 'db',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
